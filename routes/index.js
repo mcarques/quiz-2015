26cfd381aa2bc20a81bc6656dbca503bc989144b
Manuel Carques <yo@manuelcarques.com>
2015-08-01 22:37:19 +0200

    Crear comentarios

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* Página de inicio */
 router.get('/', function(req, res) {
@@ -14,14 +15,19 @@ router.get('/author', quizController.author);
 // Autoload decomandos con quizId
 router.param('quizId', quizController.load);	//autoload :quizId
 
+
 // Definición de rutas de /quizes
-router.get('/quizes',                     	quizController.index);
-router.get('/quizes/:quizId(\\d+)',       	quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
-router.get('/quizes/new',					quizController.new);
-router.post('/quizes/create',				quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',    quizController.edit);
-router.put('/quizes/:quizId(\\d+)',         quizController.update);
-router.delete('/quizes/:quizId(\\d+)',      quizController.destroy);
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
+
+// Definición de rutas de comentarios
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
 
 module.exports = router;
