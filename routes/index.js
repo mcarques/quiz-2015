1e5fe6aee5f9b934b6cbe5f5d6a220a252f20e1e
Manuel Carques <yo@manuelcarques.com>
2015-08-02 21:37:00 +0200

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/author', quizController.author);
 
 // Autoload decomandos con quizId
 router.param('quizId', quizController.load);	//autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -32,7 +33,9 @@ router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quiz
 router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
-router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+													sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
